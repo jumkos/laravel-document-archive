8631d40132a220eef8d825215f7cd1b02f77a9e2
jumkos <hendrazxc@gmail.com>
2025-09-11 15:45:47 +0700

    perbaikan route

--- app/Models/Lecturer.php
+++ app/Models/Lecturer.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Casts\HashId;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('study-programs', App\Http\Controllers\StudyProgramController::class)->parameters(['study_program' => 'id']);
-Route::apiResource('document-types', App\Http\Controllers\DocumentTypeController::class)->parameters(['document_type' => 'id']);
+Route::apiResource('study-programs', App\Http\Controllers\StudyProgramController::class)->parameters(['study-programs' => 'id']);
+Route::apiResource('document-types', App\Http\Controllers\DocumentTypeController::class)->parameters(['document-types' => 'id']);
 Route::apiResource('certificate-letters', App\Http\Controllers\CertificateLetterController::class)->parameters(['certificate-letters' => 'id']);
-Route::apiResource('outgoing-letters', App\Http\Controllers\OutgoingLetterController::class)->parameters(['outgoing_letter' => 'id']);
-Route::apiResource('incoming-letters', App\Http\Controllers\IncomingLetterController::class)->parameters(['incoming_letter' => 'id']);
-Route::apiResource('supervisor-decrees', App\Http\Controllers\SupervisorDecreeController::class)->parameters(['supervisor_decree' => 'id']);
-Route::apiResource('decree-letters', App\Http\Controllers\DecreeLetterController::class)->parameters(['decree_letter' => 'id']);
-Route::apiResource('examiner-decrees', App\Http\Controllers\ExaminerDecreeController::class)->parameters(['examiner_decree' => 'id']);
-Route::apiResource('theses', App\Http\Controllers\ThesisController::class)->parameters(['thesis' => 'id']);
-Route::apiResource('diplomas', App\Http\Controllers\DiplomaController::class)->parameters(['diploma' => 'id']);
-Route::apiResource('lecturers', App\Http\Controllers\LecturerController::class)->parameters(['lecturer' => 'id']);
-Route::apiResource('students', App\Http\Controllers\StudentController::class)->parameters(['student' => 'id']);
+Route::apiResource('outgoing-letters', App\Http\Controllers\OutgoingLetterController::class)->parameters(['outgoing-letters' => 'id']);
+Route::apiResource('incoming-letters', App\Http\Controllers\IncomingLetterController::class)->parameters(['incoming-letters' => 'id']);
+Route::apiResource('supervisor-decrees', App\Http\Controllers\SupervisorDecreeController::class)->parameters(['supervisor-decrees' => 'id']);
+Route::apiResource('decree-letters', App\Http\Controllers\DecreeLetterController::class)->parameters(['decree-letters' => 'id']);
+Route::apiResource('examiner-decrees', App\Http\Controllers\ExaminerDecreeController::class)->parameters(['examiner-decrees' => 'id']);
+Route::apiResource('theses', App\Http\Controllers\ThesisController::class)->parameters(['theses' => 'id']);
+Route::apiResource('diplomas', App\Http\Controllers\DiplomaController::class)->parameters(['diplomas' => 'id']);
+Route::apiResource('lecturers', App\Http\Controllers\LecturerController::class)->parameters(['lecturers' => 'id']);
+Route::apiResource('students', App\Http\Controllers\StudentController::class)->parameters(['students' => 'id']);
